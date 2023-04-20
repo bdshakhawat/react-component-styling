7a15c4ce73b4b17db8db9ef6f83d7d7b55938521
Shakhawat Hossain <mdshakwat.hossain@gmail.com>
2023-04-20 15:00:31 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './assets/css/global.css';
+import Logo from './components/Logo';
+
+ function App() {
+    return (
+        <div className="App">
+            <h1>Learn with Sumon</h1>
+            <Logo />
+        </div>
+    );
+}
+
+
+export default App;
+
+
+
